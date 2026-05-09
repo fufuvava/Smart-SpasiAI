4bdc11349229c6ec11cfaccbd8dd97303378b8fa
fufuvava <furymaghfuri@gmail.com>
2026-05-09 21:37:30 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'spasiai-v1';
+const ASSETS = [
+  '/Smart-SpasiAI/',
+  '/Smart-SpasiAI/index.html',
+  '/Smart-SpasiAI/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
